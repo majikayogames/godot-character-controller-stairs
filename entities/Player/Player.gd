41a5dad0b3f14cab706f8f70c4edb39ee1c0c8fc
majikayogames <majikayogames@github.com>
2023-12-15 04:28:10 -0500

    up

---
+++ entities/Player/Player.gd
@@ -0,0 +1,158 @@
+extends CharacterBody3D
+
+var speed
+const WALK_SPEED = 5.0
+const SPRINT_SPEED = 8.0
+const JUMP_VELOCITY = 4.8
+const SENSITIVITY = 0.004
+
+# bob variables
+const BOB_FREQ = 2.4
+const BOB_AMP = 0.08
+var t_bob = 0.0
+
+# fov variables
+const BASE_FOV = 75.0
+const FOV_CHANGE = 1.5
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = 9.8
+
+var camera_offset = Vector3(0,0,0)
+
+@export var coyote_time_frames_grace = 5
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func got_data(data):
+	position = data[0]
+	rotation = data[1]
+	last_rot = rotation
+	
+var last_rot = Vector3(0,0,0)
+	
+func _unhandled_input(event):
+	if event is InputEventMouseMotion and is_multiplayer_authority():
+		rotate_y(-event.relative.x * SENSITIVITY)
+		%Camera3D.rotate_x(-event.relative.y * SENSITIVITY)
+		%Camera3D.rotation.x = clamp(%Camera3D.rotation.x, deg_to_rad(-60), deg_to_rad(60))
+
+func _juice_camera(delta):
+	# Head bob
+	t_bob += delta * velocity.length() * float(is_on_floor())
+	%Camera3D.transform.origin = _headbob(t_bob)
+	
+	# FOV
+	var velocity_clamped = clamp(velocity.length(), 0.5, SPRINT_SPEED * 2)
+	var target_fov = BASE_FOV + FOV_CHANGE * velocity_clamped
+	%Camera3D.fov = lerp(%Camera3D.fov, target_fov, delta * 8.0)
+	
+func _headbob(time) -> Vector3:
+	var pos = Vector3.ZERO
+	pos.y = sin(time * BOB_FREQ) * BOB_AMP
+	pos.x = cos(time * BOB_FREQ / 2) * BOB_AMP
+	#to fix this need to teleport with camera child in portal for CharacterBody3D
+	return pos
+
+var _was_on_floor_last_frame = false
+var _snapped_to_stairs_last_frame = false
+func _snap_down_to_stairs_check():
+	var did_snap = false
+	if not is_on_floor() and velocity.y <= 0 and (_was_on_floor_last_frame or _snapped_to_stairs_last_frame) and $StairsBelowRayCast3D.is_colliding():
+		var body_test_result = PhysicsTestMotionResult3D.new()
+		var params = PhysicsTestMotionParameters3D.new()
+		var max_step_down = -0.5
+		params.from = self.global_transform
+		params.motion = Vector3(0,max_step_down,0)
+		if PhysicsServer3D.body_test_motion(self.get_rid(), params, body_test_result):
+			var translate_y = body_test_result.get_travel().y
+			self.position.y += translate_y
+			apply_floor_snap()
+			did_snap = true
+
+	_was_on_floor_last_frame = is_on_floor()
+	_snapped_to_stairs_last_frame = did_snap
+	
+@onready var _initial_separation_ray_dist = abs($StepUpSeparationRay_F.position.z)
+var _last_xz_vel : Vector3 = Vector3(0,0,0)
+func _rotate_step_up_separation_ray():
+	var xz_vel = velocity * Vector3(1,0,1)
+	
+	if xz_vel.length() < 0.1:
+		xz_vel = _last_xz_vel
+	else:
+		_last_xz_vel = xz_vel
+		
+	var xz_f_ray_pos = xz_vel.normalized() * _initial_separation_ray_dist
+	$StepUpSeparationRay_F.global_position.x = self.global_position.x + xz_f_ray_pos.x
+	$StepUpSeparationRay_F.global_position.z = self.global_position.z + xz_f_ray_pos.z
+
+	var xz_l_ray_pos = xz_f_ray_pos.rotated(Vector3(0,1.0,0), deg_to_rad(-50))
+	$StepUpSeparationRay_L.global_position.x = self.global_position.x + xz_l_ray_pos.x
+	$StepUpSeparationRay_L.global_position.z = self.global_position.z + xz_l_ray_pos.z
+	
+	var xz_r_ray_pos = xz_f_ray_pos.rotated(Vector3(0,1.0,0), deg_to_rad(50))
+	$StepUpSeparationRay_R.global_position.x = self.global_position.x + xz_r_ray_pos.x
+	$StepUpSeparationRay_R.global_position.z = self.global_position.z + xz_r_ray_pos.z
+	
+	# To prevent character from running up walls, we do a check for how steep
+	# the slope in contact with our separation rays is
+	$StepUpSeparationRay_F/RayCast3D.force_raycast_update()
+	$StepUpSeparationRay_L/RayCast3D.force_raycast_update()
+	$StepUpSeparationRay_R/RayCast3D.force_raycast_update()
+	var max_slope_ang_dot = Vector3(0,1,0).rotated(Vector3(1.0,0,0), self.floor_max_angle).dot(Vector3(0,1,0))
+	var any_too_steep = false
+	if $StepUpSeparationRay_F/RayCast3D.is_colliding() and $StepUpSeparationRay_F/RayCast3D.get_collision_normal().dot(Vector3(0,1,0)) < max_slope_ang_dot:
+		any_too_steep = true
+	if $StepUpSeparationRay_L/RayCast3D.is_colliding() and $StepUpSeparationRay_L/RayCast3D.get_collision_normal().dot(Vector3(0,1,0)) < max_slope_ang_dot:
+		any_too_steep = true
+	if $StepUpSeparationRay_R/RayCast3D.is_colliding() and $StepUpSeparationRay_R/RayCast3D.get_collision_normal().dot(Vector3(0,1,0)) < max_slope_ang_dot:
+		any_too_steep = true
+	
+	$StepUpSeparationRay_F.disabled = any_too_steep
+	$StepUpSeparationRay_L.disabled = any_too_steep
+	$StepUpSeparationRay_R.disabled = any_too_steep
+	
+var _cur_frame = 0
+@export var _jump_frame_grace = 5
+var _last_frame_was_on_floor = -_jump_frame_grace - 1
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+		
+	# Handle Jump.
+	_cur_frame += 1
+	if is_on_floor():
+		_last_frame_was_on_floor = _cur_frame
+	if Input.is_action_just_pressed("jump") and (is_on_floor() or _cur_frame - _last_frame_was_on_floor <= _jump_frame_grace):
+		velocity.y = JUMP_VELOCITY
+		
+	# Handle Sprint.
+	if Input.is_action_pressed("sprint"):
+		speed = SPRINT_SPEED
+	else:
+		speed = WALK_SPEED
+	
+	$Control/Label.text = "is_on_floor(): " + str(is_on_floor())
+	$Control/Label.label_settings.font_color = Color.RED if not is_on_floor() else Color.GREEN
+	
+	# Get the input direction and handle the movement/deceleration.
+	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if is_on_floor():
+		if direction:
+			velocity.x = direction.x * speed
+			velocity.z = direction.z * speed
+		else:
+			velocity.x = lerp(velocity.x, direction.x * speed, delta * 7.0)
+			velocity.z = lerp(velocity.z, direction.z * speed, delta * 7.0)
+	else:
+		velocity.x = lerp(velocity.x, direction.x * speed, delta * 3.0)
+		velocity.z = lerp(velocity.z, direction.z * speed, delta * 3.0)
+	
+	_rotate_step_up_separation_ray()
+	move_and_slide()
+	_snap_down_to_stairs_check()
+	_juice_camera(delta)
